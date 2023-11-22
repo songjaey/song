5105cd84b868342efb99fdc3ed95a36896e84199
songjaey <songjaey88@gmail.com>
2023-11-22 17:38:32 +0900

    kkk

--- 23-11-22/static/javascript/array2.js
+++ 23-11-22/static/javascript/array2.js
@@ -24,8 +24,8 @@
 //     document.write( num[i] + " " );
 // }
 
-let num1 = [1,2,3];
-let num2 = [4,5,6];
+// let num1 = [1,2,3];
+// let num2 = [4,5,6];
 
 // 두 배열에 같은 인덱스 위치에 있는 숫자들끼리의 합을 출력하시오
 
@@ -35,11 +35,36 @@ let num2 = [4,5,6];
 
 // num1과 num2 배열에서 짝수에 해당하는 숫자만 num 이라는 배열에 저장하고 출력
 // num3 이라는 배열에 저장하고 출력
-let num3 = new Array();
-for (var i=0; i < num1.length; i++){
-  if( num1[i] % 2 == 0 )
-    num3.push( num1[i] );
-  if( num2[i] % 2 == 0)
-    num3.push( num2[i] ); 
+
+// let num3 = new Array();
+// for (var i=0; i < num1.length; i++){
+//   if( num1[i] % 2 == 0 )
+//     num3.push( num1[i] );
+//   if( num2[i] % 2 == 0)
+//     num3.push( num2[i] ); 
+// }
+// document.write( num3 );
+
+// 과제!!
+// 
+let name = ["주현상","윤대경","이민우","이태양","페냐","박상원","장시환"];
+let era  = [1.96, 2.45, 2.63, 3.23, 3.6, 3.65, 3.38];
+let avg_up = new Array(); var avg = 0, tot = 0;
+let avg_up_name = new Array();
+// era -> 평균 자책점
+// 7명 선수들의 평균자책점의 평균치를 구하시오
+// 평균치 이상인 선수들이 누구인지 출력하세요
+
+for(var i=0; i < name.length; i++){
+  tot += era[i];  
 }
-document.write( num3 );
+avg = tot / name.length;
+for(var i=0; i < name.length; i++){
+  if( era[i] > avg )
+    avg_up.push( name[i] );
+}
+document.write("평균은 : " + avg + "<br>");
+document.write(avg_up);
+
+
+
